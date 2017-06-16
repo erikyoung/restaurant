401c8192c182141169ed5bb72240ee9193e95a2b
erikyoung <erik.young@rmit.edu.vn>
2017-06-16 23:09:29 +0700

    added food_item for current section functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,17 @@ class HomeController < ApplicationController
   def index
   end
 
+  def menu
+  	@sections = Section.all
+
+  
+  	if params[:section_id].present?
+  		@current_section = Section.find(params[:section_id])
+  	end
+  end
+
   def contact_us
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
+ 
+
+  resources :food_items
   get 'home/index'
+
+  get 'menu' => 'home#menu'
 
   get 'contact_us' => 'home#contact_us'
 
