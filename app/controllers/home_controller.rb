228af07e5a730cb9c13577ce08a5a3dbd2c2b0fb
erikyoung <erik.young@rmit.edu.vn>
2017-06-17 00:48:20 +0700

    added current_section display and jumbotron hero to menu.html.erb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,6 @@ class HomeController < ApplicationController
 
   def menu
   	@sections = Section.all
-
-  
   	if params[:section_id].present?
   		@current_section = Section.find(params[:section_id])
   	end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,3 +1,4 @@
 class Section < ApplicationRecord
-	has_many :food_items
+	has_many :food_items, dependent: :destroy
+	validates :name, :price, presence: true
 end
